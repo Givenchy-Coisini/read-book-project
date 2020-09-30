002238e0d5dbaca8fbb84439badffa5d814b3ddb
Fuyj <2312696405@qq.com>
2020-09-30 20:58:29 +0800

    API docs

--- README.md
+++
@@ -1,2 +0,0 @@
-# read-book-project
-全栈项目,基于Vue+Element-ui+Nodejs+express+MySQL实现中后台管理项目
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'Givenchy-Coisini的技术笔记',
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+            { text: '主页', link: 'http://www.youbaobao.xyz/admin-docs/' },
+            { text: '开源项目', link: 'http://www.youbaobao.xyz/admin-docs/guide/' },
+            { text: '技术笔记', link: 'http://www.youbaobao.xyz/admin-docs/api/' },
+            { text: '关于我', link: 'http://www.youbaobao.xyz/admin-docs/api/' },
+            { text: '个人博客', link: 'http://www.youbaobao.xyz/admin-docs/api/' },
+        ]
+        // logo: '/assets/img/logo.png',
+    }
+}
